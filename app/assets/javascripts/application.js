94db26cd7e67cd7e870a19f9ccd5050337c7e05c
stella zhang <subisu99@gmail.com>
2018-08-23 10:23:12 +0800

    build groups

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap/dropdown
+//= require bootstrap/alert
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'groups#index' #這行代表把 localhost:3000/groups 這個網址設成首頁
+  resources :groups
+  # get  'welcome', to: 'topics#welcome'
+  # root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
